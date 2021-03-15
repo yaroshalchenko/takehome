cc83bf4ec34a034b941477ad53ec528fb88c2255
yhalchenko <yarosgalchenko@gmail.com>
2021-03-15 19:03:40 +0200

    Code reformatted, instructions added

--- src/main/java/com/challenge/demo/repository/QuestionRepository.java
+++ src/main/java/com/challenge/demo/repository/QuestionRepository.java
@@ -8,15 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-
-  //	@Query(value = "SELECT q.* FROM questions q WHERE q.site_id = ?1", nativeQuery = true)
   List<Question> findAllBySiteSiteId(Long siteId);
-
-  //	@Query(value = "select q.* FROM questions q JOIN site s on q.site_id = s.site_id where
-  // s.site_uuid = ?1",
-  //			nativeQuery = true)
   List<Question> findAllBySiteSiteUUID(UUID uuid);
-
-  //	@Query(value = "select q FROM Question q JOIN FETCH q.parent p JOIN FETCH p.parent pp")
-  //	List<Question> findAllFetch();
 }
